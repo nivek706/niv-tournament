1b47e8ca3c40a8eac7adda70cd2c215e6671170b
Kevin Wells <nivek706@gmail.com>
2019-06-15 22:20:45 -0500

    slightly rough working version of Challonge integration

--- dashboard/nivt-challonge.js
+++ dashboard/nivt-challonge.js
@@ -2,63 +2,127 @@
 console.log("Loading nivt-challonge.js");
 var file = "[nivt-challonge]:";
 
+var challongeID = document.getElementById('challongeID');
+var challongeKey = document.getElementById('challongeKey');
+var submitChallonge = document.getElementById('ctrl-submitChallonge');
+var refreshMatches = document.getElementById('ctrl-refreshMatches');
 var selectTournament = document.getElementById('ctrl-selectTournament');
 
 var repTournamentList = nodecg.Replicant('tournamentList');
 var repCurrTournMatches = nodecg.Replicant('currTournMatches');
 var repCurrTournParticipants = nodecg.Replicant('currTournParticipants');
 
-selectTournament.addEventListener('change', function () {
-    nodecg.sendMessage('getParticipantsByTournamentId', selectTournament.value);
-    nodecg.sendMessage('getMatchesByTournamentId', selectTournament.value);
+submitChallonge.addEventListener('click', function () {
+    var data = {};
+    data.id = challongeID.value;
+    data.key = challongeKey.value;
+    console.log(`id: ${data.id} | key: ${data.key}`);
+    nodecg.sendMessage('getTournaments', data);
+
 });
 
+refreshMatches.addEventListener('click', function () {
+    var data = {};
+    data.id = challongeID.value;
+    data.key = challongeKey.value;
+    data.tournament = selectTournament.value;
+    console.log(`id: ${data.id} | key: ${data.key}`);
+    nodecg.sendMessage('getParticipantsByTournamentId', data).then((value) => {
+        console.log("repCurrTournParticipants really ready");
+        nodecg.sendMessage('getMatchesByTournamentId', data);
+    });
+});
 
-nodecg.sendMessage('getTournaments');
+// selectTournament.addEventListener('change', function () {
+//     var data = {};
+//     data.id = challongeID.value;
+//     data.key = challongeKey.value;
+//     data.tournament = selectTournament.value;
+//     console.log(`id: ${data.id} | key: ${data.key}`);
+//     nodecg.sendMessage('getParticipantsByTournamentId', data).then((value) => {
+//         console.log("repCurrTournParticipants really ready");
+//         nodecg.sendMessage('getMatchesByTournamentId', data);
+//     });
+// });
+
+
+// nodecg.sendMessage('getTournaments');
 repTournamentList.on('change', function (newValue, oldValue) {
-    console.log(file + "challonge oldValue: " + oldValue);
-    console.log(file + "challonge newValue: " + newValue);
-    console.log(file + "typeof newValue: " + typeof (newValue));
+    // console.log(file + "challonge oldValue: " + oldValue);
+    // console.log(file + "challonge newValue: " + newValue);
+    // console.log(file + "typeof newValue: " + typeof (newValue));
     var tournSelectorContent = "<option value=\"none\">------</option>";
     var tournList = document.getElementById("ctrl-selectTournament");
     for (var i = 0; i < newValue.length; i++) {
         var tournament = newValue[i].tournament;
-        console.log("name: " + tournament.name + "| ID: " + tournament.id);
+        // console.log("name: " + tournament.name + "| ID: " + tournament.id);
         tournSelectorContent = tournSelectorContent.concat("<option value=\"" + tournament.id + "\">" + tournament.name + "</option>")
     }
     tournList.innerHTML = tournSelectorContent;
 });
 
 repCurrTournParticipants.on('change', function (newValue, oldValue) {
-    console.log(file + "repCurrTournParticipants old: " + oldValue);
-    console.log(file + "repCurrTournParticipants new: " + newValue);
+    // console.log(file + "repCurrTournParticipants old: " + oldValue);
+    // console.log(file + "repCurrTournParticipants new: " + newValue);
     var eleAllParticipants = document.getElementById("participants-all");
-    console.log(eleAllParticipants);
+    // console.log(eleAllParticipants);
     var participantHTML = "";
     for (var i = 0; i < newValue.length; i++) {
-        console.log(newValue[i].participant);
+        // console.log(newValue[i].participant);
         var participant = newValue[i].participant;
         if (participant.active) {
-            participantHTML = participantHTML.concat("<div>" + participant.name + "</div>");
+            participantHTML = participantHTML.concat("<div>" + participant.name + " | " + participant.id + "</div>");
         }
     }
     eleAllParticipants.innerHTML = participantHTML;
 });
 
 repCurrTournMatches.on('change', function (newValue, oldValue) {
-    console.log(file + "repCurrTournMatches old: " + oldValue);
-    console.log(file + "repCurrTournMatches new: " + newValue);
-    var eleAllMatches = document.getElementById("matches-all");
-    console.log(eleAllMatches);
-    var matchesHTML = "";
-    for (var i = 0; i < newValue.length; i++) {
-        console.log(newValue[i].match);
-        var match = newValue[i].match;
-        var roundType = "Winners";
-        if (match.round < 0) {
-            roundType = "Losers";
+    console.log(file + "repCurrTournMatches changed: " + newValue);
+    NodeCG.waitForReplicants(repCurrTournParticipants).then(() => {
+        console.log("repCurrTournParticipants ready to go");
+
+        var eleAllMatches = document.getElementById("matches-all");
+        var matchesHTML = "";
+
+        for (var i = 0; i < newValue.length; i++) {
+            // console.log(newValue[i].match);
+            var match = newValue[i].match;
+            var roundType = "Winners";
+            if (match.round < 0) {
+                roundType = "Losers";
+            }
+            console.log("Checking participants for match: " + match.id);
+
+            var team1Name = getTeamNameByID(match.player1_id, repCurrTournParticipants.value);
+            var team2Name = getTeamNameByID(match.player2_id, repCurrTournParticipants.value);
+            matchesHTML = matchesHTML.concat("<div>" + roundType + " Round " + Math.abs(match.round) + ", match " + match.identifier + " | " + team1Name + " vs " + team2Name + "</div>");
         }
-        matchesHTML = matchesHTML.concat("<div>" + roundType + " Round " + Math.abs(match.round) + ", match " + match.identifier + "</div>");
-    }
-    eleAllMatches.innerHTML = matchesHTML;
+        eleAllMatches.innerHTML = matchesHTML;
+    });
 });
+
+function getTeamNameByID(teamID, participantArray) {
+    // console.log("In getTeamNameByID");
+    // console.log(participantArray);
+    // console.log("Looking for teamID: " + teamID);
+    // for (let key of participantArray) {
+    //     console.log(key);
+    //     for (let key2 of key) {
+    //         console.log(key2);
+    //     }
+    // }
+    // var retVal = participantArray.find(function(element) {
+    //     return element.participant["id"] == teamID;
+    // });
+
+    for (let team of participantArray) {
+        // console.log("checking team: " + team.participant["id"]);
+        if (team.participant["id"] == teamID) {
+            // console.log("success");
+            return team.participant["name"];
+        }
+    }
+    return "TBD";
+
+}
--- extension/ChallongeConnector.js
+++ extension/ChallongeConnector.js
@@ -1,9 +1,4 @@
-console.log("Hello World")
-var api_key = "";
-var url = 'https://nivek706:' + api_key + '@api.challonge.com/v1/';
-
 const request = require('request');
-
 const nodecg = require('./util/nodecg-api-context.js').get();
 
 var repTournamentList = nodecg.Replicant('tournamentList');
@@ -11,7 +6,8 @@ var repCurrTournParticipants = nodecg.Replicant('currTournParticipants');
 var repCurrTournMatches = nodecg.Replicant('currTournMatches');
 
 nodecg.listenFor('getTournaments', data => {
-  console.log(data);
+  // console.log(data);
+  var url = `https://${data.id}:${data.key}@api.challonge.com/v1/`;
   request.get(url + 'tournaments', { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
     // console.log(res);
@@ -20,50 +16,33 @@ nodecg.listenFor('getTournaments', data => {
   });
 });
 
-nodecg.listenFor('getParticipantsByTournamentId', data => {
-  console.log("getParticipantsByTournamentId: " + data);
-  request.get(url + "tournaments/" + data + "/participants", 
-  {json: true}, 
-  (err, res, body) => {
-    if (err) { return console.log(err); }
-    // console.log(body);
-    // console.log(res);
-    repCurrTournParticipants.value = body;
-  });
+nodecg.listenFor('getParticipantsByTournamentId', (data, ack) => {
+  // console.log("getParticipantsByTournamentId: " + data);
+  var url = `https://${data.id}:${data.key}@api.challonge.com/v1/`;
+  request.get(url + "tournaments/" + data.tournament + "/participants",
+    { json: true },
+    (err, res, body) => {
+      if (err) {
+        ack(err);
+        return console.log(err);
+      }
+      // console.log(body);
+      // console.log(res);
+      repCurrTournParticipants.value = body;
+      if (ack && !ack.handled) {
+        ack(null);
+      }
+    });
 });
 
 nodecg.listenFor('getMatchesByTournamentId', data => {
-  console.log("getMatchesByTournamentId: " + data);
-  request.get(url + "tournaments/" + data + "/matches",
-  {json: true},
-  (err, res, body) => {
-    if (err) { return console.log(err); }
-    console.log(body);
-    repCurrTournMatches.value = body;
-  });
+  // console.log("getMatchesByTournamentId: " + data);
+  var url = `https://${data.id}:${data.key}@api.challonge.com/v1/`;
+  request.get(url + "tournaments/" + data.tournament + "/matches",
+    { json: true },
+    (err, res, body) => {
+      if (err) { return console.log(err); }
+      // console.log(body);
+      repCurrTournMatches.value = body;
+    });
 });
-/*
-const axios = require('axios');
-let config = {
-  headers: {
-    'Content-Type': 'application/json'
-  }
-}
-
-axios.get(url, config)
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
-/*
-var request = new XMLHttpRequest();
-request.open("GET", "https://nivek706:"+api_key+"@api.challonge.com/v1/tournaments");
-request.onload = function() {
-  var data = JSON.parse(this.response);
-  console.log(data);
-}
-request.send();
-*/
